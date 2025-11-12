d9449b4ff7f196f3edb2e208e0e7ad8f111b2ec6
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-11-12 05:14:34 -0800

    created first server successfully

---
+++ node/firstServer.js
@@ -0,0 +1,10 @@
+let http = require("http");
+
+let server = http.createServer((req, res) => {
+  console.log("Request received", req);
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
