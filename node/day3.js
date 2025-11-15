9a428a48f33f01394123b4f75b5497e1349e3296
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-11-14 22:10:24 -0800

    some best practices and learning

--- node/day3.js
+++ node/day3.js
@@ -2,6 +2,12 @@ let http = require("http");
 
 let server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Complete Node js</title></head>");
+  res.write("<body><h1>Welcome to complete coding</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 const PORT = 3000;
