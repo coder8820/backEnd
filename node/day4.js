ad8586cdf79f2ee41a056baae0c8d2d6430b37a7
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-11-17 07:17:38 -0800

    little changes

--- node/day4.js
+++ node/day4.js
@@ -3,14 +3,42 @@ const http = require("http");
 let server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
     res.write("<h1>This is the home page</h1>");
     res.write("<form action='/submit-details' method='POST>");
-    res.write('<label for="name">Name:</label>');
     res.write(
-      "<input type='text' id='name' name='name' placeholder='enter your name'/>"
+      "<input type='text' name='username' placeholder='Enter your name'>"
     );
     res.write("<button type='submit'>Submit</button>");
     res.write("</form>");
+    res.write("</html>");
+    return res.end();
+  } else if (req.url === "/submit-details" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const name = parsedBody.split("=")[1];
+      res.setHeader("Content-Type", "text/html");
+      res.write("<html>");
+      res.write("<body>");
+      res.write(`<h1>Thank you, ${decodeURIComponent(name)}!</h1>`);
+      res.write("</body>");
+      res.write("</html>");
+      return res.end();
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<h1>404 - Page Not Found</h1>");
+    res.write("</body>");
+    res.write("</html>");
     return res.end();
   }
 });
