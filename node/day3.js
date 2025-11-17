d8e71a75b86cb50e2dd023851cb47830f239fe3b
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-11-16 20:56:53 -0800

    some request and responce

--- node/day3.js
+++ node/day3.js
@@ -2,12 +2,20 @@ let http = require("http");
 
 let server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>Complete Node js</title></head>");
-  res.write("<body><h1>Welcome to complete coding</h1></body>");
-  res.write("</html>");
-  res.end();
+  if (req.url === "/") {
+    res.write("<h1>This is the home page</h1>");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("<h1>This is the about page</h1>");
+    res.end();
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Complete Node js</title></head>");
+    res.write("<body><h1>Welcome to complete coding</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 const PORT = 3000;
