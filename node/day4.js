2ab0b2259edc132b118cd430dd01ce0d60192897
coder8820 <kumail112113@gmail.com>
2025-11-18 09:34:57 -0800

    fs module used to write file

--- node/day4.js
+++ node/day4.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 let server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
@@ -13,9 +14,9 @@ let server = http.createServer((req, res) => {
     res.write(
       "<input type='text' name='username' placeholder='Enter your name'>"
     );
-    res.write("<label for='gender'>Gender:</label>");
+    res.write("<br/><label for='gender'>Gender:</label>");
     res.write('<input value="male" id="male" name="gender" type="radio">');
-    res.write('<label for="male">Male</label>');
+    res.write('<br/><label for="male">Male</label>');
     res.write('<input value="female" id="female" name="gender" type="radio">');
     res.write('<label for="female">Female</label>');
     res.write("<input type='submit' value='submit'>");
@@ -23,28 +24,13 @@ let server = http.createServer((req, res) => {
     res.write("</body>");
     res.write("</html>");
     return res.end();
-  } else if (req.url === "/submit-details" && req.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const username = parsedBody.split("&")[0].split("=")[1];
-      const gender = parsedBody.split("&")[1].split("=")[1];
-      console.log("Username:", decodeURIComponent(username));
-      console.log("Gender:", decodeURIComponent(gender));
-      res.statusCode = 302;
-      res.setHeader("Location", "/");
-      return res.end();
-    });
-  } else {
-    res.statusCode = 404;
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Page Not Found</title></head>");
-    res.write("<body><h1>404 - Page Not Found</h1></body>");
-    res.write("</html>");
+  } else if (
+    req.url.toLowerCase() === "/submit-details" &&
+    req.method === "POST"
+  ) {
+    fs.writeFileSync("userDetails.txt", "Sample User Details");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
 });
