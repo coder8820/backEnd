8917357dd6e84fc6ed95282712dd091ec12995f4
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-12-08 20:30:23 -0800

    some logic here

--- node/app.js
+++ node/app.js
@@ -1,3 +1,18 @@
 const http = require("http");
 
 const server = http.createServer();
+
+server.listen(3000);
+
+server.on("listening", () => {
+  console.log("Server is listening on port 3000");
+});
+
+server.on("error", (error) => {
+  console.error("Error occurred:", error);
+});
+server.on("request", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello, World!\n");
+});
