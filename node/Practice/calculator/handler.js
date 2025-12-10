27728d8dae88c222cd674a6eb9568e1bcb995b91
coder8820 <kumail112113@gmail.com>
2025-12-10 14:45:48 -0800

    handler working properly

--- node/Practice/calculator/handler.js
+++ node/Practice/calculator/handler.js
@@ -1,9 +1,23 @@
 const requestHandler = (req, res) => {
   // Your request handling logic here
   console.log(req.url, req.method);
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello from the calculator server!\n");
+  // res.statusCode = 200;
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Practice set</title></head>");
+    res.write("<body><h1>Welcome to the Calculator App</h1>");
+    res.write("<a href='./calculator'>Go to Calculator</a>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Practice set</title></head>");
+  res.write("<body><h1>404 page does not exist</h1></body>");
+  res.write("</html>");
 };
 
-module.exports = requestHandler;
+// Export the requestHandler function
+exports.requestHandler = requestHandler;
