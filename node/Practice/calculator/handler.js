3108cfee36a2b65026d83a5c73fd525bb1677319
coder8820 <kumail112113@gmail.com>
2025-12-10 14:21:45 -0800

    creating connections

--- node/Practice/calculator/handler.js
+++ node/Practice/calculator/handler.js
@@ -1,3 +1,9 @@
 const requestHandler = (req, res) => {
   // Your request handling logic here
+  console.log(req.url, req.method);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello from the calculator server!\n");
 };
+
+module.exports = requestHandler;
