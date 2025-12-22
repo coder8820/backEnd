5d4cd39145fcf391dd81f21867648cb3f9cfdadd
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-12-22 06:09:43 -0800

    some logic remove using ai

--- node/Practice/calculator/sum.js
+++ node/Practice/calculator/sum.js
@@ -3,8 +3,8 @@ const sumHandler = (req, res) => {
   let sum;
   let sub;
   let multiplication;
-  let division;
-  req.on("data", (chunk) => {body.push(chunk);
+  req.on("data", (chunk) => {
+    body.push(chunk);
     console.log("2. Data event came")
   });
   req.on("end", () => {
@@ -16,11 +16,9 @@ const sumHandler = (req, res) => {
     sum = num1 + num2;
     sub = num1 - num2;
     multiplication = num1 * num2;
-    division = num1 / num2;
     console.log(sum);
     console.log(sub);
     console.log(multiplication);
-    console.log(division);
     console.log("4. Before sending response");
     res.setHeader("Content-Type", "text/html");
     res.write(`
@@ -31,7 +29,6 @@ const sumHandler = (req, res) => {
             <p>The sum of ${num1} and ${num2} is <strong>${sum}</strong></p>
             <p>The difference when ${num2} is subtracted from ${num1} is <strong>${sub}</strong></p>
             <p>The product of ${num1} and ${num2} is <strong>${multiplication}</strong></p>
-            <p>The quotient when ${num1} is divided by ${num2} is <strong>${division}</strong></p>
             <p>------------------------------------------------</p>
             <a href="/calculator">Perform another calculation</a>
             <h3>OR</h3>
