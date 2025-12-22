91b96e161ec79ec41819b93ec8f2139bdd1661f1
kumail <kumail112113@gmail.com>
2025-12-22 06:09:43 -0800

    some logic remove using ai

--- node/Practice/calculator/app.js
+++ node/Practice/calculator/app.js
@@ -7,5 +7,5 @@ const server = http.createServer(requestHandler);
 const PORT = 3000;
 
 server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`my Server is running on http://localhost:${PORT}`);
 });
