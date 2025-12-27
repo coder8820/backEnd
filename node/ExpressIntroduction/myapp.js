9f9554f653da54f048c2478f36a23b723ba3608b
kumail <kumail112113@gmail.com>
2025-12-27 22:02:23 +0500

    understainding express js

---
+++ node/ExpressIntroduction/myapp.js
@@ -0,0 +1,27 @@
+// Errors and Debugging in Node.js
+// This script demonstrates error handling and debugging techniques in Node.js
+
+// first creating  a server using http module
+
+const http = require("http");
+
+const testingSyntex = require("./syntex");
+const testingRuntime = require("./runtime");
+const testingLogical = require("./logical");
+const calculateAre = require("./practice");
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    // testingSyntex();
+    // testingRuntime();
+    // testingLogical();
+    calculateAre(10, 20);
+    res.end("Hello from the server!");
+
+})
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
