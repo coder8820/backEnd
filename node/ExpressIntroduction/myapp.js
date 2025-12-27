898f74b3947f52e04c3864d242c37855dd44dc0b
kumail <kumail112113@gmail.com>
2025-12-27 23:09:12 +0500

    done for today

--- node/ExpressIntroduction/myapp.js
+++ node/ExpressIntroduction/myapp.js
@@ -1,5 +1,5 @@
 // Core module
-const http = require("http");
+// const http = require("http");
 
 // External module or Third party module
 const express = require("express");
@@ -11,12 +11,21 @@ const app = express();
 
 app.use((req, res, next) => {
     console.log("Came in first middleware", req.url, req.method);
+    res.send("<h1>Hello this responce from First middleware</h1>");
+    next();
 })
 
-const server = http.createServer(app)
+app.use((req, res, next) => {
+    // requestHandler(req, res);
+    console.log("Came back from request handler");
+    res.send("<h1>Hello this responce from Second middleware</h1>");
 
+    next();
+});
+
+
+// const server = http.createServer(app)
 const PORT = 3000;
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 })
