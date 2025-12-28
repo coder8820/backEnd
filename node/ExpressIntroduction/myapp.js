cb0797d359801011298fe71569ef3fdf45a229fb
kumail <kumail112113@gmail.com>
2025-12-28 15:43:34 +0500

    debugging some logical errors

--- node/ExpressIntroduction/myapp.js
+++ node/ExpressIntroduction/myapp.js
@@ -9,18 +9,18 @@ const requestHandler = require("./user")
 
 const app = express();
 
+app.use(express.json());// to parse json data coming from client side and populate in req.body
+
 app.use((req, res, next) => {
     console.log("Came in first middleware", req.url, req.method);
-    res.send("<h1>Hello this responce from First middleware</h1>");
+    // res.send("<h1>Hello this responce from First middleware</h1>");
     next();
 })
 
 app.use((req, res, next) => {
     // requestHandler(req, res);
-    console.log("Came back from request handler");
+    console.log("Came second from request handler");
     res.send("<h1>Hello this responce from Second middleware</h1>");
-
-    next();
 });
 
 
