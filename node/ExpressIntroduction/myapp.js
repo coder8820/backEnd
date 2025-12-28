0c66f65f184c7c8a65e6526fd235cd9292e60d20
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-12-28 16:33:20 +0500

    order is matter in nextjs middleware rendering

--- node/ExpressIntroduction/myapp.js
+++ node/ExpressIntroduction/myapp.js
@@ -7,21 +7,28 @@ const express = require("express");
 // Local module
 const requestHandler = require("./user")
 
+
 const app = express();
 
 app.use(express.json());// to parse json data coming from client side and populate in req.body
 
-app.use((req, res, next) => {
-    console.log("Came in first middleware", req.url, req.method);
+app.use('/', (req, res, next) => {
+    console.log("Came from first middleware", req.url, req.method);
     // res.send("<h1>Hello this responce from First middleware</h1>");
     next();
 })
 
-app.use((req, res, next) => {
+app.post("/submit-details", (req, res, next) => {
     // requestHandler(req, res);
     console.log("Came second from request handler");
     res.send("<h1>Hello this responce from Second middleware</h1>");
 });
+
+app.use("/", (req, res, next) => {
+    console.log("Came from another middleware", req.url, req.method);
+    res.send("<h1>Wecome to proCoder website.</h1>");
+})
+
 
 
 // const server = http.createServer(app)
