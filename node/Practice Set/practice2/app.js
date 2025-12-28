da0c4ac0c63bafeb3e9b13671df38da00518c485
kumail <kumail112113@gmail.com>
2025-12-28 21:36:48 +0500

    added two dummy middleware

--- node/Practice Set/practice2/app.js
+++ node/Practice Set/practice2/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("First Dummy Middleware");
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log("Second Dummy Middleware");
+    next();
+})
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
