257be277c72434d2cfabd30b7a78e084809fa30b
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-12-28 21:39:27 +0500

    console the url and method

--- node/Practice Set/practice2/app.js
+++ node/Practice Set/practice2/app.js
@@ -3,17 +3,17 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-    console.log("First Dummy Middleware");
+    console.log("First Dummy Middleware", req.url, req.method);
     next();
 })
 
 app.use((req, res, next) => {
-    console.log("Second Dummy Middleware");
+    console.log("Second Dummy Middleware", req.url, req.method);
     next();
 })
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 })
