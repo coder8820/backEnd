6e84a33342dead4c7719ed9ce19e9e9f9aad57bb
kumail <kumail112113@gmail.com>
2025-12-29 22:16:16 +0500

    created profile of html file

--- node/projects/airbnb/routes/adminRouter.js
+++ node/projects/airbnb/routes/adminRouter.js
@@ -1,17 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const adminRouter = express.Router();
 
 
 
 adminRouter.get("/dashboard", (req, res, next) => {
-    res.send(`
-        <h1>Admin Dashboard</h1>
-        <p>Welcome to the Admin Dashboard of Airbnb application.</p>
-        <img src="/images/airbnb.jpg" alt="Airbnb Logo" width="200" height="200" /><br/><br/>
-        <a href="/">Go Back to Home</a><br/><br/>
-        <a href="/contact-us">Contact Us</a>
-    `);
+    res.sendFile(path.join(__dirname, '..', 'views', 'admin.html'));    
 })
 
 module.exports = adminRouter;
