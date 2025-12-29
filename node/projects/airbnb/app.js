76567e31308f947af44c442b2db5b0d922ea9ad1
kumail <kumail112113@gmail.com>
2025-12-29 18:39:04 +0500

    added a add-home route in second middleware

--- node/projects/airbnb/app.js
+++ node/projects/airbnb/app.js
@@ -3,8 +3,30 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
+    console.log(req.url, req.method);
+    next();
+})
+
+app.get("/", (req, res, next) => {
     console.log("First Middleware", req.url, req.method);
-    res.send("<h1>Im working on airbnb Website.</h1>");
+    res.send(`
+        <h1>Welcome to Airbnb</h1>
+        <a href="/add-home">Add Home </a>
+        `);
+})
+app.get("/add-home", (req, res, nextd) => {
+    console.log("Add Home Middleware", req.url, req.method);
+    res.send(`
+        <h1>Add your Home</h1>
+        <form action="/add-home" method="POST">
+            <input type="text" name="name" placeholder="Enter your home name" /><br/><br/>
+            <input type="text" name="location" placeholder="Enter your home location" /><br/><br/>
+            <input type="number" name="price" placeholder="Enter your home price" /><br/><br/>
+            <button type="submit">Submit</button>
+        </form>
+        
+        `)
+
 })
 
 const PORT = 3000;
