70b8b35820c7e5bc079cef425cf6ac5d6c95e011
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-12-29 14:55:08 +0500

    Understainding Express deeply

---
+++ node/ExpressDeeply/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("First Dummy Middleware", req.url, req.method);
+    next();
+})
+
+app.use((req, res, next) => {
+    console.log("Second Dummy Middleware", req.url, req.method);
+    next();
+})
+
+// app.use(((req, res, next) => {
+//     console.log("This is the Third Middleware for post request");
+//     res.send("<h1>This is the third middleware</h1>");
+// }))
+
+app.get("/", (req, res, next) => {
+    console.log("Handling the GET request");
+    res.send("<h1>This is the GET request</h1>");
+})
+
+app.get("/contact-us", (req, res, next) => {
+    console.log("Handling the Contact Us GET request");
+    res.send(`
+        <h1>Contact Us Page</h1>
+        <form action="/contact-us" method="POST">
+            <input type="text" name="name" placeholder="Enter your name" /><br/><br/>
+            <input type="email" name="email" placeholder="Enter your email" /><br/><br/>
+            <input type="password" name="password" placeholder="Enter your password" /><br/><br/>
+            <button type="submit">Submit</button>
+        </form>
+    `);
+})
+
+app.post("/contact-us", (req, res, next) => {
+    console.log("Handling the Contact Us POST request");
+    res.send("<h1>Thank you for contacting us!</h1>");
+})
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
