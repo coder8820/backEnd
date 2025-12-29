2af8da824b808d0764837887cb59a444113e0996
kumail <kumail112113@gmail.com>
2025-12-29 23:01:27 +0500

    everything is working porfect

--- node/projects/airbnb/routes/adminRouter.js
+++ node/projects/airbnb/routes/adminRouter.js
@@ -6,7 +6,7 @@ const adminRouter = express.Router();
 
 
 adminRouter.get("/dashboard", (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'admin.html'));    
+    res.sendFile(path.join(__dirname, '..', 'views', 'admin.html'));
 })
 
 module.exports = adminRouter;
