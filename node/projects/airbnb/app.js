a7f9589ac81c2ee03880a3a645ee3c3e2403df0d
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2025-12-29 19:02:11 +0500

    home page register successfull middleware created

--- node/projects/airbnb/app.js
+++ node/projects/airbnb/app.js
@@ -7,24 +7,35 @@ app.use((req, res, next) => {
     next();
 })
 
+app.use(express.urlencoded());
+
 app.get("/", (req, res, next) => {
     res.send(`
         <h1>Welcome to Airbnb</h1>
         <a href="/add-home">Add Home </a>
         `);
 })
-app.get("/add-home", (req, res, nextd) => {
+app.get("/add-home", (req, res, next) => {
+    console.log("is this showing... ", req.body)
     res.send(`
-        <h1>Add your Home</h1>
+        <h1>Register you home here</h1>
         <form action="/add-home" method="POST">
-            <input type="text" name="name" placeholder="Enter your home name" /><br/><br/>
+            <input type="text" name="housename" placeholder="Enter your home name" /><br/><br/>
             <input type="text" name="location" placeholder="Enter your home location" /><br/><br/>
             <input type="number" name="price" placeholder="Enter your home price" /><br/><br/>
             <button type="submit">Submit</button>
         </form>
-        
-        `)
-
+        `);
+})
+app.post("/add-home", (req, res, next) => {
+    console.log("is this showing... ", req.body)
+    res.send(`
+        <h1>Home Registered Successfully</h1>
+        <p>Home Name: ${req.body.housename}</p>
+        <p>Location: ${req.body.location}</p>
+        <p>Price: ${req.body.price}</p>
+        <a href="/">Go Back to Home</a>
+        `);
 })
 
 const PORT = 3000;
