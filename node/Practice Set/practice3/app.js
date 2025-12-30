a5cbe36cbd73de7971d4e947cdbf06980594573f
kumail <kumail112113@gmail.com>
2025-12-30 17:54:13 +0500

    added styling to every files

--- node/Practice Set/practice3/app.js
+++ node/Practice Set/practice3/app.js
@@ -1,14 +1,25 @@
+// core modules
+const path = require('path');
 // External Modules
 const express = require('express');
 
 // local modules
 const homeRouter = require('./routes/homeRouter')
 const contactRouter = require('./routes/contactRouter')
+const rootDir = require('./utils/pathUtils')
 
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+
 app.use(homeRouter)
 app.use(contactRouter)
+
+// 404 Not found route handler
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+})
+
 
 const PORT = 3000;
 
