5e791fdc0e0ee7f5d29b41df1504ef5bd3bf457b
kumail <kumail112113@gmail.com>
2026-01-01 16:41:39 +0500

    there are many changes and updates here

--- node/DynamicUi ejs/routes/userRouter.js
+++ node/DynamicUi ejs/routes/userRouter.js
@@ -2,13 +2,17 @@
 const express = require('express');
 // Core Module
 const path = require('path');
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views"); //
 
 // local module
 const userRouter = express.Router();
 const rootDir = require('../utils/pathUtils');
 
 userRouter.get("/profile", (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'profile.html'));
+    res.render('profile', { pageTitle: 'User Profile' });
 })
 
 
