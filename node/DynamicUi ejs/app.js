eca3bf9ce44eec3004a303a268f2b5102f42d6c0
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-01 15:52:24 +0500

    reusing the header and navbar in every files

--- node/DynamicUi ejs/app.js
+++ node/DynamicUi ejs/app.js
@@ -32,7 +32,7 @@ app.get("/", (req, res, next) => {
 
 // 404 Not found route handler
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found' });
 })
 
 
