68d7bceb98beba6fd67713f2be1976badeac7c5c
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-02 17:06:03 +0500

    post addhome added in controler

--- node/mvc/Controllers/homes.js
+++ node/mvc/Controllers/homes.js
@@ -1,3 +1,18 @@
 exports.getAddHome = (req, res, next) => {
     res.render('addhome', { pageTitle: 'Add New Home', currentPage: 'add-home' });
 }
+
+
+exports.postAddHome = (req, res, next) => {
+    registeredHomes.push(
+        {
+            name: req.body.housename,
+            location: req.body.location,
+            description: req.body.description,
+            price: req.body.price,
+            image: req.body.imageurl,
+            rating: req.body.rating
+        }
+    );
+    res.render('homeadded', { pageTitle: 'Home Added', currentPage: 'add-home' });
+}
--- node/mvc/routes/hostrouter.js
+++ node/mvc/routes/hostrouter.js
@@ -13,19 +13,7 @@ hostRouter.get("/add-home", hostController.getAddHome)
 
 const registeredHomes = [];
 
-hostRouter.post("/add-home", (req, res, next) => {
-    registeredHomes.push(
-        {
-            name: req.body.housename,
-            location: req.body.location,
-            description: req.body.description,
-            price: req.body.price,
-            image: req.body.imageurl,
-            rating: req.body.rating
-        }
-    );
-    res.render('homeadded', { pageTitle: 'Home Added', currentPage: 'add-home' });
-})
+hostRouter.post("/add-home", hostController.postAddHome)
 
 
 exports.hostRouter = hostRouter;
