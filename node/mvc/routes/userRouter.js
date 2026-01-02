96d38188b4518971c3449a11fb084c8c86c4f414
kumail <kumail112113@gmail.com>
2026-01-02 16:02:21 +0500

    Learnng MVC Architecture for better development

---
+++ node/mvc/routes/userRouter.js
@@ -0,0 +1,22 @@
+// External modules which is used for routing purpose
+const express = require('express');
+// Core Module
+const path = require('path');
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views"); //
+
+// local module
+const userRouter = express.Router();
+const rootDir = require('../utils/pathUtils');
+
+userRouter.get("/profile", (req, res, next) => {
+    res.render('profile', { pageTitle: 'User Profile', currentPage: 'profile' });
+})
+
+userRouter.get("/settings", (req, res) => {
+    res.render("settings", { title: "Settings", currentPage: 'settings' });
+});
+
+module.exports = userRouter;
