21eb566f231ed1eba0b09ca6177e2ac015fed91b
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-03 19:39:15 +0500

    it is intresting to work on model

--- node/mvc/models/home.js
+++ node/mvc/models/home.js
@@ -3,6 +3,20 @@ const registeredHomes = [];
 
 module.exports = class Home {
     constructor(housename, location, description, price, rating, imageurl) {
-
+        this.housename = housename,
+            this.location = location,
+            this.description = description,
+            this.price = price,
+            this.rating = rating,
+            this.imageurl = imageurl
     }
+
+    save() {
+        registeredHomes.push(this)
+    }
+
+    static fetchAll() {
+        return registeredHomes
+    }
+
 }
