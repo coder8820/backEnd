8a72e1f69bbbf1b8d0e225dded1fc06082fbf0ad
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-05 18:25:00 +0500

    Working Dynamic path part 2

---
+++ node/mvcDynamic/Controllers/storeController.js
@@ -0,0 +1,70 @@
+const Home = require("../models/home");
+
+// Home page
+exports.getHomes = (req, res, next) => {
+    Home.fetchAll(registeredHomes =>
+        res.render('store/home-list', { registeredHomes: registeredHomes, pageTitle: 'Home', currentPage: 'home-list' }));
+}
+// home index
+exports.getIndex = (req, res, next) => {
+    Home.fetchAll(registeredHomes =>
+        res.render('store/home-list', { registeredHomes: registeredHomes, pageTitle: 'airbnb Home', currentPage: 'Index home' }));
+}
+
+// Dashboard
+exports.getDashboard = (req, res, next) => {
+    res.render('store/admin', { pageTitle: 'Admin Dashboard', currentPage: 'dashboard' });
+}
+
+// profile
+exports.getProfile = (req, res, next) => {
+    res.render('host/profile', { pageTitle: 'User Profile', currentPage: 'profile' });
+}
+
+// booking
+exports.getBooking = (req, res, next) => {
+    const homeId = req.params.id;
+    Home.fetchById(homeId, (home) => {
+        if (!home) {
+            return res.status(404).render('404', { pageTitle: '404' });
+        }
+        res.render('store/booking', { home: home, pageTitle: 'Bookings', currentPage: 'Bookings' })
+    })
+}
+
+// all bookings list
+exports.getBookings = (req, res, next) => {
+    res.render('store/bookings-list', { pageTitle: 'My Bookings', currentPage: 'bookings' })
+}
+
+
+// setting 
+
+exports.getSetting = (req, res) => {
+    res.render("store/settings", { title: "Settings", currentPage: 'settings' });
+}
+
+// Favorite list
+exports.getFavorits = (req, res, next) => {
+    Home.fetchAll(registeredHomes =>
+        res.render('store/favorit-list', { registeredHomes: registeredHomes, pageTitle: 'Favorites', currentPage: 'favorites' }));
+}
+
+// Add home - GET
+exports.getAddHome = (req, res, next) => {
+    res.render('host/addhome', { pageTitle: 'Add Home', currentPage: 'add-home' });
+}
+
+// Add home - POST
+exports.postAddHome = (req, res, next) => {
+    const { housename, location, description, price, rating, imageurl } = req.body;
+    const home = new Home(housename, location, description, price, rating, imageurl);
+    home.save();
+    res.redirect('/');
+}
+
+// Host home list
+exports.getHostHomeList = (req, res, next) => {
+    Home.fetchAll(registeredHomes =>
+        res.render('host/host-home-list', { registeredHomes: registeredHomes, pageTitle: 'My Listings', currentPage: 'listings' }));
+}
---
+++ node/mvcDynamic/app.js
@@ -0,0 +1,40 @@
+// External modules which is used to create server
+const express = require('express');
+const path = require('path');
+
+// local module which is used for related to routes
+const storeRouter = require('./routes/storeRouter');
+const adminRouter = require('./routes/adminRouter');
+const hostRouter = require('./routes/hostrouter');
+const errorController = require('./Controllers/errors')
+
+const app = express();
+
+
+// Serve static files (CSS, JS, images) from public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.set('view engine', 'ejs');// setting up ejs as template engine
+app.set('views', 'views'); // setting up views folder for ejs templates
+
+
+
+
+app.use(express.urlencoded());
+app.use(storeRouter);
+app.use("/admin", adminRouter);
+app.use("/host", hostRouter);
+
+const hostController = require('./Controllers/storeController')
+
+app.get("/", hostController.getHomes)
+
+// 404 Not found route handler
+app.use(errorController.pageNotFound)
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}`);
+})
