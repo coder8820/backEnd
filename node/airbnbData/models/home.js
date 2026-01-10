dc2f5f1c9576a93a599dce8fab80e814895a9e37
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-10 21:06:11 +0500

    now almost working properly

--- node/airbnbData/models/home.js
+++ node/airbnbData/models/home.js
@@ -14,7 +14,11 @@ module.exports = class Home {
   }
 
   save() {
-    return db.execute('INSERT INTO homes (houseName,price, location,rating,photoUrl,description)VALUES(?, ?, ?, ?, ?, ?)', [this.houseName, this.price, this.location, this.rating, this.photoUrl, this.description])
+    if (this.id) {// Update
+      return db.execute('UPDATE homes SET houseName=?, price=?, location=?, rating=?, photoUrl=?, description=? WHERE id=?', [this.houseName, this.price, this.location, this.rating, this.photoUrl, this.description, this.id])
+    } else {// Insert   
+      return db.execute('INSERT INTO homes (houseName,price, location,rating,photoUrl,description)VALUES(?, ?, ?, ?, ?, ?)', [this.houseName, this.price, this.location, this.rating, this.photoUrl, this.description])
+    }
   }
 
   static fetchAll() {
