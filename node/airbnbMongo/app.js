5b7184375c8b686a86c0f0bdf1be64b1ddc5037e
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-11 17:45:37 +0500

    now everything is fine

--- node/airbnbMongo/app.js
+++ node/airbnbMongo/app.js
@@ -9,6 +9,7 @@ const storeRouter = require("./routes/storeRouter")
 const hostRouter = require("./routes/hostRouter")
 const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
+const mongoConnect = require('./utils/databaseUtil');
 
 
 
@@ -26,6 +27,9 @@ app.use(express.static(path.join(rootDir, 'public')))
 app.use(errorsController.pageNotFound);
 
 const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on address http://localhost:${PORT}`);
-});
+mongoConnect(client => {
+  console.log(client);
+  app.listen(PORT, () => {
+    console.log(`Server running on address http://localhost:${PORT}`);
+  });
+})
--- node/airbnbMongo/utils/databaseUtil.js
+++ node/airbnbMongo/utils/databaseUtil.js
@@ -2,12 +2,11 @@ const mongo = require('mongodb');
 
 const MongoClient = mongo.MongoClient;
 
-const MONGO_URL = "mongodb+srv://coder:<db_password>@cluster0.7it96af.mongodb.net/?appName=Cluster0";
+const MONGO_URL = "mongodb+srv://coder:coder@cluster0.7it96af.mongodb.net/?appName=Cluster0";
 
 
 const mongoConnect = (callback) => {
     MongoClient.connect(MONGO_URL).then(client => {
-        console.log(client);
         callback(client);
     }).catch(err => {
         console.log('Error while connecting to mongoDb', err)
