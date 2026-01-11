18af33ab1cafe14a7de2f967e29095ff46dd74b6
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-11 17:05:52 +0500

    imported mongodb

--- node/airbnbMongo/utils/databaseUtil.js
+++ node/airbnbMongo/utils/databaseUtil.js
@@ -1,10 +1,1 @@
-const mysql = require("mysql2")
-
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'coder',
-    database: 'airbnb'
-})
-
-module.exports = pool.promise();
+const mongo = require('mongodb');
