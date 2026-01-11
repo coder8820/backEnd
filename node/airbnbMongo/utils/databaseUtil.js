0ccff27df4cf4effbf89ffa628bc0b547bbeaedc
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-11 17:27:26 +0500

    try to connecting to mongo atlas

--- node/airbnbMongo/utils/databaseUtil.js
+++ node/airbnbMongo/utils/databaseUtil.js
@@ -1,1 +1,17 @@
 const mongo = require('mongodb');
+
+const MongoClient = mongo.MongoClient;
+
+const MONGO_URL = "mongodb+srv://coder:<db_password>@cluster0.7it96af.mongodb.net/?appName=Cluster0";
+
+
+const mongoConnect = (callback) => {
+    MongoClient.connect(MONGO_URL).then(client => {
+        console.log(client);
+        callback(client);
+    }).catch(err => {
+        console.log('Error while connecting to mongoDb', err)
+    })
+}
+
+module.exports = mongoConnect;
