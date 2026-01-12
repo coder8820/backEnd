a873826c3632f766dc446bdda6f1916295c38c92
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-12 21:25:18 +0500

    mongoose is very intresting to learn

--- node/mongoose/app.js
+++ node/mongoose/app.js
@@ -9,7 +9,7 @@ const storeRouter = require("./routes/storeRouter")
 const hostRouter = require("./routes/hostRouter")
 const rootDir = require("./utils/pathUtil");
 const errorsController = require("./controllers/errors");
-const { mongoConnect } = require('./utils/databaseUtil');
+const { default: mongoose } = require('mongoose');
 
 
 
@@ -27,8 +27,13 @@ app.use(express.static(path.join(rootDir, 'public')))
 app.use(errorsController.pageNotFound);
 
 const PORT = 3001;
-mongoConnect(() => {
+const DB_PATH = "mongodb+srv://coder:coder@cluster0.7it96af.mongodb.net/airbnb?appName=Cluster0";
+
+mongoose.connect(DB_PATH).then(() => {
+  console.log("Successfully Connect to mongoose")
   app.listen(PORT, () => {
     console.log(`Server running on address http://localhost:${PORT}`);
   });
-})
+}).catch((err) => {
+  console.log('Error while connecting to mongoose', err)
+});
