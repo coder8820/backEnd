11e7325f4a515e3ced680d8226e8e7c486c84faf
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-13 23:41:20 +0500

    Now I am learning cookies

---
+++ node/cookies/controllers/storeController.js
@@ -0,0 +1,80 @@
+const Favourite = require("../models/favourite");
+const Home = require("../models/home");
+
+exports.getIndex = (req, res, next) => {
+  Home.find().then(registeredHomes => {
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb Home",
+      currentPage: "index",
+    })
+  })
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.find().then(registeredHomes => {
+    res.render("store/home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Homes List",
+      currentPage: "Home",
+    })
+  });
+};
+
+exports.getBookings = (req, res, next) => {
+  res.render("store/bookings", {
+    pageTitle: "My Bookings",
+    currentPage: "bookings",
+  })
+};
+
+exports.getFavouriteList = (req, res, next) => {
+  Favourite.find().populate('houseId').then(favourites => {
+    const favouriteHomes = favourites.map((fav) => fav.houseId)
+    res.render("store/favourite-list", {
+      favouriteHomes: favouriteHomes,
+      pageTitle: "My Favourites",
+      currentPage: "favourites",
+    })
+  });
+};
+
+exports.postAddToFavourite = (req, res, next) => {
+  const homeId = req.body._id;
+  const fav = new Favourite({ houseId: homeId })
+  fav.save().then(reseult => {
+    console.log('Fav added ', reseult)
+  }).catch(err => {
+    console.log('error while add to favourite', err)
+  }).finally(() => {
+    res.redirect("/favourites");
+  })
+}
+
+exports.postRemoveFromFavourite = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Favourite.deleteOne({ houseId: homeId }).then(reseult => {
+    console.log('Fav removed ', reseult)
+  }).catch(err => {
+    console.log('error while removing from favourite', err)
+  }).finally(() => {
+    res.redirect("/favourites");
+  })
+}
+
+exports.getHomeDetails = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Home.findById(homeId).then(home => {
+    if (!home) {
+      console.log("Home not found");
+      res.redirect("/homes");
+    } else {
+      res.render("store/home-detail", {
+        home: home,
+        pageTitle: "Home Detail",
+        currentPage: "Home",
+      });
+    }
+  })
+};
+
