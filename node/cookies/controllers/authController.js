4b1e284e5cc23d903b6867fffb54e3ff6a0b8ff4
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-14 23:01:54 +0500

    authenticaiton learning

--- node/cookies/controllers/authController.js
+++ node/cookies/controllers/authController.js
@@ -3,10 +3,13 @@ exports.getLogin = (req, res, next) => {
   console.log("login functionality");
   res.render("auth/login", {
     pageTitle: 'login form',
-    currentPage: 'login'
+    currentPage: 'login',
+    isLoggedIn: false,
   })
 }
 
 exports.postLogin = (req, res, next) => {
+  console.log(req.body);
+  req.isLoggedIn = true;
   res.redirect("/")
 }
--- node/cookies/controllers/storeController.js
+++ node/cookies/controllers/storeController.js
@@ -7,6 +7,7 @@ exports.getIndex = (req, res, next) => {
       registeredHomes: registeredHomes,
       pageTitle: "airbnb Home",
       currentPage: "index",
+      isLoggedIn: req.isLoggedI
     })
   })
 };
@@ -17,6 +18,7 @@ exports.getHomes = (req, res, next) => {
       registeredHomes: registeredHomes,
       pageTitle: "Homes List",
       currentPage: "Home",
+      isLoggedIn: req.isLoggedIn
     })
   });
 };
@@ -25,6 +27,7 @@ exports.getBookings = (req, res, next) => {
   res.render("store/bookings", {
     pageTitle: "My Bookings",
     currentPage: "bookings",
+    isLoggedIn: req.isLoggedIn
   })
 };
 
@@ -35,6 +38,7 @@ exports.getFavouriteList = (req, res, next) => {
       favouriteHomes: favouriteHomes,
       pageTitle: "My Favourites",
       currentPage: "favourites",
+      isLoggedIn: req.isLoggedIn
     })
   });
 };
@@ -73,6 +77,7 @@ exports.getHomeDetails = (req, res, next) => {
         home: home,
         pageTitle: "Home Detail",
         currentPage: "Home",
+        isLoggedIn: req.isLoggedIn
       });
     }
   })
