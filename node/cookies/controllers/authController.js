0092cfaa0f0d1086ebc7970d29786172f49e6053
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-14 22:26:35 +0500

    created a login form

--- node/cookies/controllers/authController.js
+++ node/cookies/controllers/authController.js
@@ -1,18 +1,8 @@
-const Home = require("../models/home");
 
-exports.getAddHome = (req, res, next) => {
-  res.render("host/edit-home", {
-    pageTitle: "Add Home to airbnb",
-    currentPage: "addHome",
-    editing: false,
-  });
-};
-
-// exports.getLogin = (req, res, next) => {
-//   console.log("login functionality");
-//   res.render("host/login", {
-//     pageTitleL: 'login form',
-//     currentPage: 'login',
-//     editing: false
-//   })
-// }
+exports.getLogin = (req, res, next) => {
+  console.log("login functionality");
+  res.render("auth/login", {
+    pageTitle: 'login form',
+    currentPage: 'login'
+  })
+}
