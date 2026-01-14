56c92bb1a6469cafa1c21c958957ff4e67c56365
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-14 20:04:21 +0500

    added the funtionality

--- node/cookies/controllers/authController.js
+++ node/cookies/controllers/authController.js
@@ -8,81 +8,11 @@ exports.getAddHome = (req, res, next) => {
   });
 };
 
-exports.getEditHome = (req, res, next) => {
-  const homeId = req.params.homeId;
-  const editing = req.query.editing === 'true';
-
-  Home.findById(homeId).then(home => {
-    if (!home) {
-      return res.redirect("/host/host-home-list");
-    }
-    // console.log(homeId, editing, home);
-    res.render("host/edit-home", {
-      home: home,
-      pageTitle: "Edit your Home",
-      currentPage: "host-homes",
-      editing: editing,
-    });
-  });
-};
-
-exports.getHostHomes = (req, res, next) => {
-  Home.find().then(registeredHomes => {
-    res.render("host/host-home-list", {
-      registeredHomes: registeredHomes,
-      pageTitle: "Host Homes List",
-      currentPage: "host-homes",
-    })
-  });
-};
-
-exports.postAddHome = (req, res, next) => {
-  const { houseName, price, location, rating, photoUrl, description } = req.body;
-  const home = new Home({ houseName, price, location, rating, photoUrl, description });
-  home.save().then(() => {
-    console.log("Home saved successfully.")
-  });
-  res.redirect("/host/host-home-list");
-};
-
-
-
-exports.postEditHome = (req, res, next) => {
-  const { id, houseName, price, location, rating, photoUrl, description } = req.body;
-
-  Home.findById(id)
-    .then(home => {
-      if (!home) {
-        console.log("❌ Home NOT FOUND for id:", id);
-        return res.redirect("/host/host-home-list");
-      }
-
-      home.houseName = houseName;
-      home.price = price;
-      home.location = location;
-      home.rating = rating;
-      home.photoUrl = photoUrl;
-      home.description = description;
-
-      return home.save();
-    })
-    .then(() => {
-      console.log("✅ Home updated successfully");
-      res.redirect("/host/host-home-list");
-    })
-    .catch(err => {
-      console.log("Error while updating home", err);
-      res.redirect("/host/host-home-list");
-    });
-};
-
-
-exports.postDeleteHome = (req, res, next) => {
-  const homeId = req.params.homeId;
-  console.log('Came to delete ', homeId);
-  Home.findByIdAndDelete(homeId).then(() => {
-    res.redirect("/host/host-home-list");
-  }).catch(error => {
-    console.log('Error while editing ', error)
+exports.getLogin = (req, res, next) => {
+  console.log("login functionality");
+  res.render("host/login", {
+    pageTitleL: 'login form',
+    currentPage: 'login',
+    editing: false
   })
-};
+}
