245096aee93c833ed664e2dd26839f8324ae9187
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-17 23:12:26 +0500

    validated email and password

--- node/auth-authorization/controllers/authController.js
+++ node/auth-authorization/controllers/authController.js
@@ -36,8 +36,24 @@ exports.postSignup = [
   
   check('lastName')
     .matches(/^[A-Za-z\s]*$/)
-    .withMessage('First name should only be alphabets')
+    .withMessage('First name should only be alphabets'),
   
+  check('email').isEmail().withMessage('Enter a valid email address')
+    .normalizeEmail(),
+  
+  check('password').isLength({ min: 8 }).withMessage('must me 8 character long')
+    .matches(/[A-Za-z]/).withMessage('at-least one Capital letter')
+    .matches(/[0 - 9]/).withMessage('add one number')
+    .matches(/[@#!$%^&]/)
+    .trim(),
+  
+  check('confimPassword').trim()
+    .custom((value, { req })=> {
+    if(value !== req.body.password) {
+        throw new Error('password do not match')
+      }
+     return true;
+}) ,
 ,(req, res, next) => {
    console.log(req.body);
   res.redirect("/")
