7464f249ce546d3c2332bf28ebf0a1ca65c42862
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-17 23:43:24 +0500

    error shoing

--- node/auth-authorization/controllers/authController.js
+++ node/auth-authorization/controllers/authController.js
@@ -1,4 +1,4 @@
-const { check } = require("express-validator");
+const { check, validationResult } = require("express-validator");
 
 exports.getLogin = (req, res, next) => {
   console.log("login functionality");
@@ -53,8 +53,27 @@ exports.postSignup = [
         throw new Error('password do not match')
       }
      return true;
-}) ,
-,(req, res, next) => {
-   console.log(req.body);
-  res.redirect("/")
-}]
+    }),
+  check('userType').notEmpty().withMessage('user type is required')
+    .isIn(['guest', 'host']).withMessage('Invalid user type'),
+  check('terms').notEmpty().withMessage('Please accept the terms and conditions')
+    .custom((value, { req }) => {
+      if (value !== 'on') {
+        throw new Error('Please accept the terms and conditions')
+      }
+      return true
+    })
+  , (req, res, next) => {
+    const { firstName, lastName, email, password, userType } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: 'SignUp',
+        currentPage: 'signup',
+        isLoggedIn: false,
+        errors: errors.array().map(err => err.map),
+        oldInput:{firstName,lastName,email,password,userType}
+      })
+    }
+    res.redirect("/login")
+}] 
