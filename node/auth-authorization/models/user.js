32420fd7fcbfcd00c4c8ecd0710b25702d1668c5
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-18 20:38:49 +0500

    now passowrd encryption started

--- node/auth-authorization/models/user.js
+++ node/auth-authorization/models/user.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  firstName: { String, required: [true, 'First name is required'] },
-  lastName: {String},
+  firstName: {type: String, required: [true, 'First name is required'] },
+  lastName: {type:String},
   email: { type: String, required: [true, 'Email is required'], unique: true },
   password: { type: String, required: [true, 'Password is required'] },
   userType:{type:String, enum:['guest','host'],default:'guest'}
