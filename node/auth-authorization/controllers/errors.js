514ed651db29441606d3f249690d7e1454f5530e
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-21 20:59:56 +0500

    controller working properly as authorized

--- node/auth-authorization/controllers/errors.js
+++ node/auth-authorization/controllers/errors.js
@@ -1,8 +1,12 @@
+const user = require("../models/user");
+
 exports.pageNotFound = (req, res, next) => {
   res
     .status(404)
     .render("404", {
       pageTitle: "Page Not Found",
       currentPage: "404",
+      // isLoggedIn: req.session.isLoggedIn
+      // user: req.session.user
     });
 };
