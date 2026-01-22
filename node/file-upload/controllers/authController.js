09481e07abea8e0752c256f9766db6d97037fa18
Kumail Abbas <137616046+coder8820@users.noreply.github.com>
2026-01-22 22:05:54 +0500

    Now i am learning file uploading logics

---
+++ node/file-upload/controllers/authController.js
@@ -0,0 +1,137 @@
+const { check, validationResult } = require("express-validator");
+const User = require("../models/user");
+const bcrypt = require('bcryptjs')
+
+exports.getLogin = (req, res, next) => {
+  console.log("login functionality");
+  res.render("auth/login", {
+    pageTitle: 'login form',
+    currentPage: 'login',
+    isLoggedIn: false,
+    oldInput: { email: '', password: '' },
+    user: {}
+  })
+}
+
+exports.postLogin = async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).render('auth/login', {
+      pageTitle: 'login',
+      currentPage: 'login',
+      isLoggedIn: false,
+      errors: ['User does not exist'],
+      oldInput: { email, password: '' },
+      user:{}
+    })
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(422).render('auth/login', {
+      pageTitle: 'Login',
+      currentPage: 'login',
+      isLoggedIn: false,
+      errors: ['Invalid Password'],
+      oldInput: { email, password: '' },
+      user:{}
+    })
+  }
+  req.session.user = {
+    _id: user._id.toString(),  // store as string
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    userType: user.userType
+  };
+  req.session.isLoggedIn = true;
+  await req.session.save(err => {
+    if (err) console.log(err);
+    res.redirect('/');
+  });
+}
+
+exports.postLogout = (req, res) => {
+  // res.clearCookie('isLoggedIn');
+  req.session.destroy(() => {
+    res.redirect('/login')
+  })
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    pageTitle: 'signup page',
+    currentPage: 'signup',
+    isLoggedIn: false,
+    errors: [],
+    oldInput: { firstName: '', lastName: '', email: '', userType: '' },
+    user: {}
+  })
+}
+exports.postSignup = [
+  check('firstName').trim().isLength({ min: 2 })
+    .withMessage('First name should be 2 character long')
+    .matches(/^[A-Za-z\s]+$/)
+    .withMessage('First name should only be alphabets'),
+  
+  check('lastName')
+    .matches(/^[A-Za-z\s]*$/)
+    .withMessage('First name should only be alphabets'),
+  
+  check('email').isEmail().withMessage('Enter a valid email address')
+    .normalizeEmail(),
+  
+  check('password').isLength({ min: 8 }).withMessage('must me 8 character long')
+    .matches(/[A-Z]/).withMessage('At least one capital letter')
+    .matches(/[0-9]/).withMessage('Add at least one number')
+    .matches(/[@#!$%^&]/).withMessage('Add one special character')
+    .trim(), 
+  
+  check('confirmPassword').trim()
+    .custom((value, { req })=> {
+    if(value !== req.body.password) {
+        throw new Error('Confirm password must match password')
+      }
+     return true;
+    }),
+  check('userType').notEmpty().withMessage('user type is required')
+    .isIn(['guest', 'host']).withMessage('Invalid user type'),
+  
+  check('terms').notEmpty().withMessage('Please accept the terms and conditions')
+    .custom((value, { req }) => {
+      if (value !== 'on') {
+        throw new Error('Please accept the terms and conditions')
+      }
+      return true
+    })
+  , (req, res, next) => {
+    const { firstName, lastName, email, password, userType } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).render("auth/signup", {
+        pageTitle: 'SignUp',
+        currentPage: 'signup',
+        isLoggedIn: false,
+        errors: errors.array().map(err => err.msg),
+        oldInput: { firstName, lastName, email, password, userType },
+        user:{}
+      })
+    }
+
+    bcrypt.hash(password, 12).then(hashedPassword => {
+      const user = new User({ firstName, lastName, email, password: hashedPassword, userType });
+      return user.save();
+    }).then(() => {
+      res.redirect('/login')
+    }).catch(err => {
+      return res.status(422).render('auth/signup', {
+        pageTitle: 'Signup',
+        currentPage: 'signup',
+        isLoggedIn: false,
+        errors: [err.message],
+        oldInput: { firstName, lastName, email, password, userType },
+        user:{}
+      })
+    })
+
+  }] 
---
+++ node/file-upload/controllers/storeController.js
@@ -0,0 +1,82 @@
+// const Favourite = require("../models/favourite");
+const Home = require("../models/home");
+const User = require("../models/user");
+
+exports.getIndex = (req, res, next) => {
+  console.log('Session value', req.session)
+  Home.find().then(registeredHomes => {
+    res.render("store/index", {
+      registeredHomes: registeredHomes,
+      pageTitle: "airbnb Home",
+      currentPage: "index",
+    })
+  })
+};
+
+exports.getHomes = (req, res, next) => {
+  Home.find().then(registeredHomes => {
+    res.render("store/home-list", {
+      registeredHomes: registeredHomes,
+      pageTitle: "Homes List",
+      currentPage: "Home",
+    })
+  });
+};
+
+exports.getBookings = (req, res, next) => {
+  res.render("store/bookings", {
+    pageTitle: "My Bookings",
+    currentPage: "bookings",
+  })
+};
+
+exports.getFavouriteList = async (req, res, next) => {
+  const userId = req.session.user._id;
+  const user = await User.findById(userId).populate('favourites');
+  console.log('user', user)
+    res.render("store/favourite-list", {
+      favouriteHomes: user.favourites,
+      pageTitle: "My Favourites",
+      currentPage: "favourites",
+    })
+};
+
+exports.postAddToFavourite = async (req, res, next) => {
+  const homeId = req.body._id;
+  const userId = req.session.user._id;
+  const user = await User.findById(userId);
+  if (!user.favourites.includes(homeId)) {
+    user.favourites.push(homeId);
+    await user.save()
+  }
+    res.redirect("/favourites");
+}
+
+exports.postRemoveFromFavourite = async (req, res, next) => {
+  const homeId = req.params.homeId;
+  const userId = req.session.user._id;
+  const user = await User.findById(userId);
+  console.log('deleted favourite',user)
+  if(user.favourites.includes(homeId)) {
+    user.favourites = user.favourites.filter(fav => fav.toString() !== homeId);
+    await user.save();
+  }
+    res.redirect("/favourites");
+}
+
+exports.getHomeDetails = (req, res, next) => {
+  const homeId = req.params.homeId;
+  Home.findById(homeId).then(home => {
+    if (!home) {
+      console.log("Home not found");
+      res.redirect("/homes");
+    } else {
+      res.render("store/home-detail", {
+        home: home,
+        pageTitle: "Home Detail",
+        currentPage: "Home",
+      });
+    }
+  })
+};
+
